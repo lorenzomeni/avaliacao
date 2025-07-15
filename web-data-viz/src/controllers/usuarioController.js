eade0b772947df4599438ab10469762e4ae0fe37
lorenzomeni <lorenzo.rodrigues@sptech.school>
2025-07-15 11:39:47 -0300

    refazendo primeira parte e concluindo-a

---
+++ web-data-viz/src/controllers/usuarioController.js
@@ -0,0 +1,99 @@
+var usuarioModel = require("../models/usuarioModel");
+var aquarioModel = require("../models/aquarioModel");
+
+function autenticar(req, res) {
+    var email = req.body.emailServer;
+    var senha = req.body.senhaServer;
+
+    if (email == undefined) {
+        res.status(400).send("Seu email está undefined!");
+    } else if (senha == undefined) {
+        res.status(400).send("Sua senha está indefinida!");
+    } else {
+
+        usuarioModel.autenticar(email, senha)
+            .then(
+                function (resultadoAutenticar) {
+                    console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
+                    console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
+
+                    if (resultadoAutenticar.length == 1) {
+                        console.log(resultadoAutenticar);
+
+                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
+                            .then((resultadoAquarios) => {
+                                if (resultadoAquarios.length > 0) {
+                                    res.json({
+                                        id: resultadoAutenticar[0].id,
+                                        email: resultadoAutenticar[0].email,
+                                        nome: resultadoAutenticar[0].nome,
+                                        senha: resultadoAutenticar[0].senha,
+                                        aquarios: resultadoAquarios
+                                    });
+                                } else {
+                                    res.status(204).json({ aquarios: [] });
+                                }
+                            })
+                    } else if (resultadoAutenticar.length == 0) {
+                        res.status(403).send("Email e/ou senha inválido(s)");
+                    } else {
+                        res.status(403).send("Mais de um usuário com o mesmo login e senha!");
+                    }
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+
+}
+
+function cadastrar(req, res) {
+    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+    var livro = req.body.nomeServer;
+    var autor = req.body.autorServer;
+    var compra = req.body.compraServer;
+    var venda = req.body.vendaServer;
+    var estoque = req.body.estoqueServer;
+    var genero = req.body.generoServer;
+
+    // Faça as validações dos valores
+    if (livro == undefined) {
+        res.status(400).send("Seu livro está undefined!");
+    } else if (autor == undefined) {
+        res.status(400).send("Seu autor está undefined!");
+    } else if (compra == undefined) {
+        res.status(400).send("Sua compra está undefined!");
+    } else if (venda == undefined) {
+        res.status(400).send("Sua venda está undefined!"); 
+    } else if (estoque == undefined) {
+        res.status(400).send("Sua empresa a vincular está undefined!");
+    } else if (genero == undefined) {
+        res.status(400).send("Seu gênero está undefined!")
+    } else {
+        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+        usuarioModel.cadastrar(livro, autor, compra, venda, estoque, genero)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
+
+module.exports = {
+    autenticar,
+    cadastrar
+}
